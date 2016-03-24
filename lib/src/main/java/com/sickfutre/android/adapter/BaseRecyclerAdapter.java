91d4aed6be6ab5a7a4706ed88f02acecfa9c9586
Alex Dzeshko <a.dzeshko@gmail.com>
2016-03-24 23:38:25 +0300

    groups dev

--- lib/src/main/java/com/sickfutre/android/adapter/BaseRecyclerAdapter.java
+++ lib/src/main/java/com/sickfutre/android/adapter/BaseRecyclerAdapter.java
@@ -1,15 +1,21 @@
 package com.sickfutre.android.adapter;
 
+import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Checkable;
 import android.widget.Filter;
 import android.widget.Filterable;
 
 import com.sickfutre.android.util.Function;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -19,10 +25,30 @@ public abstract class BaseRecyclerAdapter<T, VH extends RecyclerView.ViewHolder>
         extends RecyclerView.Adapter<VH>
         implements Filterable {
 
+    public static final int CHOICE_MODE_NONE = 0;
+    public static final int CHOICE_MODE_SINGLE = 1;
+    public static final int CHOICE_MODE_MULTIPLE = 2;
+
+    @IntDef({CHOICE_MODE_NONE, CHOICE_MODE_SINGLE, CHOICE_MODE_MULTIPLE})
+    @Retention(RetentionPolicy.SOURCE)
+    @interface ChoiceMode {
+    }
+
     private List<T> mOriginalValues;
 
     private Comparator<? super T> mSortComparator;
     private Function<T, Boolean> mFilter;
+
+    @ChoiceMode
+    private int choiceMode = CHOICE_MODE_NONE;
+    private SparseBooleanArray selectedItems;
+    private OnItemSelectionChangeListener<T> itemSelectedListener;
+
+
+    public interface OnItemSelectionChangeListener<T> {
+        void onSelect(boolean isSelected, T data, int position);
+    }
+
     /**
      * Lock used to modify the content of . Any write operation
      * performed on the array should be synchronized on this lock. This lock is also
@@ -33,6 +59,7 @@ public abstract class BaseRecyclerAdapter<T, VH extends RecyclerView.ViewHolder>
 
     public BaseRecyclerAdapter() {
         mOriginalValues = new ArrayList<T>();
+        selectedItems = new SparseBooleanArray();
     }
 
     public BaseRecyclerAdapter(@NonNull List<T> values) {
@@ -47,6 +74,12 @@ public abstract class BaseRecyclerAdapter<T, VH extends RecyclerView.ViewHolder>
 
     @Override
     public void onBindViewHolder(VH holder, int position) {
+        boolean selected = selectedItems.get(position, false);
+        if (holder.itemView instanceof Checkable) {
+            ((Checkable) holder.itemView).setChecked(selected);
+        } else {
+            holder.itemView.setActivated(selected);
+        }
         onBindItemViewHolder(holder, getItem(position), position, getItemViewType(position));
     }
 
@@ -57,6 +90,81 @@ public abstract class BaseRecyclerAdapter<T, VH extends RecyclerView.ViewHolder>
     @Override
     public int getItemCount() {
         return mOriginalValues.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        recyclerView.setOnTouchListener(new ItemTouchListener(recyclerView));
+    }
+
+    public class ItemTouchListener implements View.OnTouchListener {
+
+        private RecyclerView recyclerView;
+
+        public ItemTouchListener(RecyclerView recyclerView) {
+            this.recyclerView = recyclerView;
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (choiceMode == CHOICE_MODE_NONE) {
+                return false;
+            }
+            View view = recyclerView.findChildViewUnder(event.getX(), event.getY());
+            if (view == null) return false;
+
+            int position = recyclerView.getChildAdapterPosition(view);
+            if (choiceMode == CHOICE_MODE_SINGLE) {
+                for (int i = 0; i < selectedItems.size(); i++) {
+                    int posToUnselect = selectedItems.keyAt(i);
+                    if (posToUnselect == position) {
+                        toggleSelection(posToUnselect);
+                    } else {
+                        if (selectedItems.get(posToUnselect)) {
+                            selectedItems.put(posToUnselect, false);
+                            notifyItemChanged(posToUnselect);
+                        }
+
+                    }
+                }
+            } else if (choiceMode == CHOICE_MODE_MULTIPLE) {
+                toggleSelection(position);
+            }
+            return false;
+        }
+    }
+
+    public void toggleSelection(int pos) {
+        if (selectedItems.get(pos, false)) {
+            selectedItems.delete(pos);
+            if (itemSelectedListener != null) {
+                itemSelectedListener.onSelect(false, getItem(pos), pos);
+            }
+        } else {
+            selectedItems.put(pos, true);
+            if (itemSelectedListener != null) {
+                itemSelectedListener.onSelect(true, getItem(pos), pos);
+            }
+        }
+        notifyItemChanged(pos);
+    }
+
+    public void clearSelections() {
+        selectedItems.clear();
+        notifyDataSetChanged();
+    }
+
+    public int getSelectedItemCount() {
+        return selectedItems.size();
+    }
+
+    public List<T> getSelectedItems() {
+        List<T> items = new ArrayList<>(selectedItems.size());
+        for (int i = 0; i < selectedItems.size(); i++) {
+            items.add(mOriginalValues.get(selectedItems.keyAt(i)));
+        }
+        return items;
     }
 
     public void add(int position, T item) {
@@ -179,6 +287,17 @@ public abstract class BaseRecyclerAdapter<T, VH extends RecyclerView.ViewHolder>
             getFilter().filter("");
         }
 
+    }
+
+    public int getChoiceMode() {
+        return choiceMode;
+    }
+
+    public void setChoiceMode(@ChoiceMode int choiceMode) {
+        this.choiceMode = choiceMode;
+        if (choiceMode == CHOICE_MODE_NONE) {
+            clearSelections();
+        }
     }
 
     protected abstract void onBindItemViewHolder(VH viewHolder, T data, int position, int type);
