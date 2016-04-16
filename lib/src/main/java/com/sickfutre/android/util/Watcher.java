33e0b95d4d0da378f38b8ad0ff347c9ce3a06126
Alex Dzeshko <a.dzeshko@gmail.com>
2016-04-16 23:18:54 +0300

    relocate watcher

---
+++ lib/src/main/java/com/sickfutre/android/util/Watcher.java
@@ -0,0 +1,18 @@
+package com.sickfutre.android.util;
+
+import android.text.TextWatcher;
+
+/**
+ * Created by Alex Dzeshko on 05-Apr-16.
+ */
+public abstract class Watcher implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+}
