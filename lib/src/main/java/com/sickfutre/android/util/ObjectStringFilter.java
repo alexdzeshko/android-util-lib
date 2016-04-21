34f5fee0423b3b85e13788af7fb712eaec5a1bf1
Alex Dzeshko <a.dzeshko@gmail.com>
2016-04-21 23:54:00 +0300

    dashboard list, summary, tabs

--- lib/src/main/java/com/sickfutre/android/util/ObjectStringFilter.java
+++ lib/src/main/java/com/sickfutre/android/util/ObjectStringFilter.java
@@ -8,40 +8,42 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public abstract class ObjectFilter<T> extends Filter {
+public abstract class ObjectStringFilter<T> extends Filter {
     private List<T> objects;
 
-    public ObjectFilter(List<T> objects) {
+    public ObjectStringFilter(List<T> objects) {
         this.objects = objects;
     }
+
+    protected abstract String getObjectStringToSearchIn(T object);
 
     protected abstract void onFiltered(List<T> values);
 
     @Override
     protected FilterResults performFiltering(final CharSequence constraint) {
         FilterResults filterResults = new FilterResults();
-        if (TextUtils.isEmpty(constraint)) {
+        if(TextUtils.isEmpty(constraint)) {
             filterResults.values = objects;
         } else {
             List<T> results = new ArrayList<>();
             final String formattedQuery = constraint.toString().toLowerCase();
             for (T t : objects) {
-                if (acceptable(t, constraint)) {
+                if (getObjectStringToSearchIn(t).toLowerCase().contains(formattedQuery)) {
                     results.add(t);
                 }
             }
-            Comparator<? super T> comparator = getComparator();
-            if (comparator != null) {
-                Collections.sort(results, comparator);
-            }
+            Collections.sort(results, new Comparator<T>() {
+                @Override
+                public int compare(T lhs, T rhs) {
+                    Integer lhsVal = getObjectStringToSearchIn(lhs).toLowerCase().indexOf(formattedQuery);
+                    int rhsVal = getObjectStringToSearchIn(rhs).toLowerCase().indexOf(formattedQuery);
+                    return lhsVal.compareTo(rhsVal);
+                }
+            });
             filterResults.values = results;
         }
         return filterResults;
     }
-
-    protected abstract Comparator<? super T> getComparator();
-
-    protected abstract boolean acceptable(T t, CharSequence constraint);
 
     @Override
     protected void publishResults(CharSequence constraint, FilterResults results) {
