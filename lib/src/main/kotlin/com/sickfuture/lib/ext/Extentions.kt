249554a9631c953c708a4ac427bf77499765d5cd
Alex Dzeshko <a.dzeshko@gmail.com>
2016-05-05 00:25:17 +0300

    sliding layout

---
+++ lib/src/main/kotlin/com/sickfuture/lib/ext/Extentions.kt
@@ -0,0 +1,6 @@
+package com.sickfuture.lib.ext
+
+/**
+ * Created by Alex Dzeshko on 05-May-16.
+ */
+fun Float.abs(): Float = Math.abs(this)
---
+++ lib/src/main/kotlin/com/sickfuture/lib/spare.kt
@@ -0,0 +1,5 @@
+package com.sickfuture.lib
+
+/**
+ * Created by Alex Dzeshko on 05-May-16.
+ */
