52b0e8600a85f7d93e82a10e2eb81cbc84ba5adf
Alex Dzeshko <a.dzeshko@gmail.com>
2016-05-10 23:20:31 +0300

    event detail dev

--- lib/src/main/java/com/sickfutre/android/util/Dates.java
+++ lib/src/main/java/com/sickfutre/android/util/Dates.java
@@ -1,10 +1,17 @@
 package com.sickfutre.android.util;
 
+import android.util.Log;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class Dates {
-
+    private static final String TAG = "Dates";
     public static Date nextYear() {
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.YEAR, 1);
@@ -42,5 +49,14 @@ public class Dates {
         cal.setTime(date);
         cal.add(Calendar.DAY_OF_YEAR, 1);
         return cal.getTime();
+    }
+
+    public static Date format(@NotNull String format, String value) {
+        try {
+            return new SimpleDateFormat(format, Locale.getDefault()).parse(value);
+        } catch (ParseException e) {
+            Log.e(TAG, "Can't parse date: "+value+" with format "+format);
+            return null;
+        }
     }
 }
